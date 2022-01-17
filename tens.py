4c4ada6160da86f97c88ab30211eabc9aee327c1
Naeem <52814859+naeem-bebit@users.noreply.github.com>
2022-01-17 23:23:50 +0800

    sort

--- tens.py
+++ tens.py
@@ -1,13 +1,15 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-df = pd.read_csv('Churn.csv')
-X = pd.get_dummies(df.drop(['Churn', 'Customer ID'], axis=1))
-y = df['Churn'].apply(lambda x: 1 if x=='Yes' else 0)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
 
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+df = pd.read_csv('Churn.csv')
+X = pd.get_dummies(df.drop(['Churn', 'Customer ID'], axis=1))
+y = df['Churn'].apply(lambda x: 1 if x=='Yes' else 0)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
 
 model = Sequential()
 model.add(Dense(units=32, activation='relu', input_dim=len(X_train.columns)))
